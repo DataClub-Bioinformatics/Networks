97b076c0446e16a69580c6a9b4480c5628b6416d
ViPoBio <victor.ponten+git@me.com>
2020-04-10 00:32:34 +0200

    added python files

---
+++ python/fetchGOdata.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Here we want to call some GO api and fetch data for a list of sent genes
+
---
+++ python/generateNetwork.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Here we want to generate a network into a json file
+
